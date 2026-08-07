8a2b6f20a06cd5d6ce4be91e5767f72866bb43fa
Praveen Kumar Gupta <pg23403@gmail.com>
2026-08-07 11:54:42 +0530

    feat: add DigitalMarketingMarquee component for displaying client logos

--- app/service/digital-marketing/components/DigitalMarketingMarquee.tsx
+++ app/service/digital-marketing/components/DigitalMarketingMarquee.tsx
@@ -25,42 +25,42 @@ export default function DigitalMarketingMarquee() {
                 loading="lazy"
                 width={100}
                 alt=""
-               height={800} />
+               height={30} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712221f1b7f58cfd9fe_Group 1597884746.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712f0df798d907c8c07_Group 1597884747.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef27122b527a12c7a225a7_Group 1597883493.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712bdcf3d7a4fee2f43_REZOTA.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef271276a33d103013fa46_Group 1597884750.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
           </div>
           <div
@@ -78,42 +78,42 @@ export default function DigitalMarketingMarquee() {
                 loading="lazy"
                 width={100}
                 alt=""
-               height={800} />
+               height={30} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712221f1b7f58cfd9fe_Group 1597884746.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712f0df798d907c8c07_Group 1597884747.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef27122b527a12c7a225a7_Group 1597883493.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712bdcf3d7a4fee2f43_REZOTA.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef271276a33d103013fa46_Group 1597884750.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
           </div>
           <div
@@ -131,42 +131,42 @@ export default function DigitalMarketingMarquee() {
                 loading="lazy"
                 width={100}
                 alt=""
-               height={800} />
+               height={30} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712221f1b7f58cfd9fe_Group 1597884746.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712f0df798d907c8c07_Group 1597884747.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef27122b527a12c7a225a7_Group 1597883493.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef2712bdcf3d7a4fee2f43_REZOTA.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/Home3_files/68ef271276a33d103013fa46_Group 1597884750.svg"
                 loading="lazy"
                 alt=""
-               width={800} height={800} style={{ width: "100%", height: "auto" }} />
+               width={800} height={30} style={{ width: "auto", height: "30px" }} />
             </div>
           </div>
         </div>

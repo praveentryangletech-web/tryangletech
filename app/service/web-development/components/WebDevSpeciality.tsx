a544f1c70a26ed934bc88b0f858979f0e9cbb1f7
Praveen Kumar Gupta <pg23403@gmail.com>
2026-08-07 10:49:28 +0530

    feat: add WebDevSpeciality component with animated client-side logo marquee

--- app/service/web-development/components/WebDevSpeciality.tsx
+++ app/service/web-development/components/WebDevSpeciality.tsx
@@ -22,42 +22,42 @@ export default function ServiceOneSpeciality() {
                 width={100}
                 alt=""
                 height={40}
-                style={{ height: "40px", width: "auto" }} />
+                style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712221f1b7f58cfd9fe_Group 1597884746.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712f0df798d907c8c07_Group 1597884747.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef27122b527a12c7a225a7_Group 1597883493.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712bdcf3d7a4fee2f43_REZOTA.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef271276a33d103013fa46_Group 1597884750.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
           </div>
           <div className="rt-text-marquee-train">
@@ -68,42 +68,42 @@ export default function ServiceOneSpeciality() {
                 width={100}
                 alt=""
                 height={40}
-                style={{ height: "40px", width: "auto" }} />
+                style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712221f1b7f58cfd9fe_Group 1597884746.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712f0df798d907c8c07_Group 1597884747.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef27122b527a12c7a225a7_Group 1597883493.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712bdcf3d7a4fee2f43_REZOTA.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef271276a33d103013fa46_Group 1597884750.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
           </div>
           <div className="rt-text-marquee-train">
@@ -114,42 +114,42 @@ export default function ServiceOneSpeciality() {
                 width={100}
                 alt=""
                 height={40}
-                style={{ height: "40px", width: "auto" }} />
+                style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712221f1b7f58cfd9fe_Group 1597884746.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712f0df798d907c8c07_Group 1597884747.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef27122b527a12c7a225a7_Group 1597883493.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef2712bdcf3d7a4fee2f43_REZOTA.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
             <div className="rt-text-marquee-iteme">
               <Image
                 src="/service-1-assets/68ef271276a33d103013fa46_Group 1597884750.svg"
                 loading="lazy"
                 alt=""
-                width={200} height={80} style={{ height: "40px", width: "auto" }} />
+                width={200} height={80} style={{ height: "30px", width: "auto" }} />
             </div>
           </div>
         </div>

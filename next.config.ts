86f1d83d4edcc02c4bccff2152d22fec980c1184
Praveen Kumar Gupta <pg23403@gmail.com>
2026-08-18 16:15:46 +0530

    refactor: update next.config.ts to structure rewrites into beforeFiles, afterFiles, and fallback arrays

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [
-      {
-        source: '/portfolio/:filename*',
-        destination: '/api/media/:filename*',
-      },
-    ];
+    return {
+      beforeFiles: [],
+      afterFiles: [
+        {
+          source: '/portfolio/:filename*',
+          destination: '/api/media/:filename*',
+        },
+      ],
+      fallback: [
+        {
+          source: '/portfolio/:filename*',
+          destination: '/api/media/:filename*',
+        },
+      ],
+    };
   },
 };
 
